74e6f797936f3e09f8f9dc094cf0ed42e2741bee
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-05-19 23:15:51 +0530

    build the issue status badge.

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,38 +1,48 @@
 import React from "react";
 import Link from "next/link";
-import { Button,Table } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
+import IssueStatusBadge from "../components/issueStatusBadge";
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
   return (
     <div className="text-black">
       <div className="mb-5">
-      <Button>
-        <Link href="/issues/new">Create Issue</Link>
-      </Button>
+        <Button>
+          <Link href="/issues/new">Create Issue</Link>
+        </Button>
       </div>
       <Table.Root variant="surface">
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell className="hidden md:table-cell">Created At</Table.ColumnHeaderCell>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created At
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
             </Table.Row>
-          </Table.Header>
-          <Table.Body>
-              {issues.map(issue=>(
-                <Table.Row key={issue.id}>
-                  <Table.Cell>{issue.title}
-                  <div className="block md:hidden">
-                    {issue.status}
-                  </div>
-                  </Table.Cell>
-                  <Table.Cell className="hidden md:table-cell">{issue.status}</Table.Cell>
-                  <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
-                </Table.Row >
-              ))}
-          </Table.Body>
-        </Table.Root>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
