26540a60ef7bd5fa18a0535e74e6c687d261a41a
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-05-24 02:52:38 +0530

    Fixed the key error in which is coming from the Navbar.

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -9,30 +9,35 @@ export const Navbar = () => {
   const currPath = usePathname();
   const links = [
     {
-      key:"1",
+      key: 1,
       label: "Dashboard",
-      herf: "/",
+      href: "/",
     },
     {
-      key:"2",
+      key: 2,
       label: "Issues",
-      herf: "/issues",
+      href: "/issues",
     },
   ];
-  
+
   return (
     <nav className="text-black border-b flex space-x-6 items-center h-14 mb-5 px-5">
       <Link href="/">
         <IoBugSharp />
       </Link>
-      <ul className="flex space-x-6 ">
+      <ul className="flex space-x-6">
         {links.map((link) => (
-          <li className={classnames({
-            "text-zinc-900":currPath===link.herf,
-            "text-zinc-500":currPath!==link.herf,
-            "hover:text-zinc-800 transition-colors":true
-          })}>
-            <Link key={link.key} href={link.herf}>{link.label}</Link>
+          <li
+            key={link.key}
+            className={classnames({
+              "text-zinc-900": currPath === link.href,
+              "text-zinc-500": currPath !== link.href,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+          >
+            <Link href={link.href}>
+              {link.label}
+            </Link>
           </li>
         ))}
       </ul>
