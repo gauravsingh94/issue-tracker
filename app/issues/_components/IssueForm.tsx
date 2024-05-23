6bb2f3301a4711dd3c545c71a40a93427e35ca20
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-05-24 02:33:23 +0530

    Added the update issue route to the update issue page.

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -34,7 +34,11 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setIsSubmitting(true);
-      await axios.post("/api/issues", data);
+      if (issue) {
+        await axios.patch("/api/issues/" + issue.id, data);
+      } else {
+        await axios.post("/api/issues", data);
+      }
       router.push("/issues");
     } catch (error) {
       setError("An unexpected error occured.");
@@ -69,7 +73,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
           <ErrorMessage>{errors.description.message}</ErrorMessage>
         )}
         <Button disabled={isSubmitting}>
-          Submit
+          {issue ? "Update Issue" : "Create Issue"}
           {isSubmitting && <Spinner />}
         </Button>
       </form>
