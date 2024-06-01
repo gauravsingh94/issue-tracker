9ebccdf25a3a47de78133d87ab29f49bf9106a9d
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-06-01 23:25:14 +0530

    Secure the application.

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,11 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import { issueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
+import authOptions from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
 
+// updating the issue
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success)
@@ -28,10 +34,15 @@ export async function PATCH(
   return NextResponse.json(updatedIssue);
 }
 
+
+// deleting the issues
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+  
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
