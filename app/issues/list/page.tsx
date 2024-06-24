fcd257e5c795f365d6988b6865eb5d8bacbe12d6
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-06-25 03:40:29 +0530

    completed the functionality to sort the columns.

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -32,10 +32,18 @@ const IssuePage = async ({ searchParams }: PropsType) => {
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
   return (
     <div className="text-black">
